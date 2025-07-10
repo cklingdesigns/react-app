b20c7d24f0a8db81c00067502c0a2c6bdb62f52b
cklingdesigns@gmail.com <cklingdesigns@gmail.com>
2025-07-10 15:36:44 -0400

    new changes

--- src/index.js
+++ src/index.js
@@ -5,16 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
-<BrowserRouter basename={process.env.NODE_ENV === 'production' ? '/react-app' : '/'}>
+<BrowserRouter basename="/react-cklingdesigns">
   <App />
 </BrowserRouter>
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
